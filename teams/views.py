52c58e84dc73775b62101bfca118d926ea3374a1
DiceMono <qrdfg@naver.com>
2019-11-15 04:19:35 +0900

    add custom permissions

--- teams/views.py
+++ teams/views.py
@@ -2,9 +2,11 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Team
 from .serializers import TeamSerializer
+from .permissions import IsLeaderOrReadCreateOnly
 
 
 class TeamViewSet(ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsLeaderOrReadCreateOnly,)
+
