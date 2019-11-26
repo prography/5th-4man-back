1bdf69fb2c2d1ad57a9a059ed69199ad15b66ea3
DiceMono <qrdfg@naver.com>
2019-11-27 01:21:01 +0900

    add feature: Comment

--- teams/urls.py
+++ teams/urls.py
@@ -1,10 +1,12 @@
 from rest_framework import routers
-from .views import TeamViewSet, TagViewSet
+from .views import TeamViewSet, TagViewSet, CommentViewSet
 
 team_router = routers.SimpleRouter()
 team_router.register(r'', TeamViewSet)
 tag_router = routers.SimpleRouter()
 tag_router.register(r'', TagViewSet)
+comment_router = routers.SimpleRouter()
+comment_router.register(r'', CommentViewSet)
 
 urlpatterns = [
 ]
